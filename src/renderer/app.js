cfa492d7711c8eb4286794adf0f00e2df401898c
Dor Shaer <85877103+DorShaer@users.noreply.github.com>
2026-07-13 00:11:01 +0300

    feat(ui): quicken Kernel's retreat into the husk

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -7084,7 +7084,7 @@ async function runOnboarding({ replay = false } = {}) {
           settle = setTimeout(() => {
             hk.classList.add('is-wiggle');
             setTimeout(() => hk.classList.remove('is-wiggle'), 640);
-          }, 2050);
+          }, 1300);
           return;
         }
         hk.classList.remove('is-shut', 'is-pop', 'is-wiggle');
